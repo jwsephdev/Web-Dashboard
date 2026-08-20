ca215a50985394b5fadc2a7051e5589be756f2b4
Jwseph <116055108+jwsephdev@users.noreply.github.com>
2026-08-20 10:49:14 +0800

    taskStatus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,34 @@ import { useState } from 'react'
 
 function Header() {
 
-  function ChangeStatus() {
-    const [taskStatus, setTaskStatus] = useState("Pending")
+  const [items, setItems] = useState([]);
+  const [newTitle, setNewTitle] = useState("")
+  const [newRole, setNewRole] = useState("")
+  const [newLocation, setNewLocation] = useState("")
+  const [newDate, setNewDate] = useState("")
+
+
+const pendingCount = items.filter((item) => item.status === "Pending").length
+const inProgressCount = items.filter((item) => item.status === "In Progress").length
+const completedCount = items.filter((item) => item.status === "Completed").length
+
+
+  function ChangeStatus({status , onChange}) {
     const [statusMenuBar, setOpenMenuBar] = useState(false)
     const toggleVisibility = () => setOpenMenuBar((prev) => !prev) 
-
     return (
     <div className='flex flex-col'>
 
-      <button className=' bg-green-500 w-40 h-10 rounded hover:bg-green-600 transition-all ease' onClick={toggleVisibility}>{taskStatus}</button>
+      <button className=' bg-green-500 w-40 h-10 rounded hover:bg-green-600 transition-all ease' onClick={toggleVisibility}>{status}</button>
 
       {statusMenuBar && (
         <div className='flex flex-col w-fit bg-gray-200 rounded-b-2xl'> 
-        <button onClick={() => {setTaskStatus("Pending"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease' >Pending</button>
-        <button onClick={() => {setTaskStatus("In Progress"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease' >In Progress</button>
-        <button onClick={() => {setTaskStatus("Completed"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease'>Completed</button>
+        <button onClick={() => {onChange("Pending"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease' >Pending</button>
+        <button onClick={() => {onChange("In Progress"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease' >In Progress</button>
+        <button onClick={() => {onChange("Completed"); setOpenMenuBar((prev) => !prev)}} className=' w-40 h-10 hover:bg-gray-400 transition-all ease'>Completed</button>
       </div>
       )}
-      
     </div>
-
     )
   } 
 
@@ -42,12 +50,7 @@ function Header() {
   setValue("");
 };
 
-  const [items, setItems] = useState([]);
-  const [newTitle, setNewTitle] = useState("")
-  const [newRole, setNewRole] = useState("")
-  const [newLocation, setNewLocation] = useState("")
-  const [newDate, setNewDate] = useState("")
-
+  
 
   const addEntry = () => {
     if (
@@ -73,9 +76,18 @@ function Header() {
     setNewDate("")
   }
 
+  const updateStatus = (index, newStatus) => {
+  setItems((prev) =>
+    prev.map((item, i) =>
+      i === index ? { ...item, status: newStatus } : item
+    )
+  )
+}
+
   return(
     <div>
       
+
       <input type='text'
        placeholder='Title..'
        value={newTitle}
@@ -104,6 +116,12 @@ function Header() {
 
       <button onClick={addEntry}>Add</button>
 
+
+    <div>
+      <h1>Pending: {pendingCount}</h1>
+      <h1>In Progress: {inProgressCount}</h1>
+      <h1>Completed: {completedCount}</h1>
+    </div>
       <div>
         <ol>
         {items.map((item, index) =>
@@ -112,12 +130,15 @@ function Header() {
            <span>Role: {item.role}</span> <br/>
            <span>Location: {item.location}</span> <br/>
            <span>Deadline: {item.date}</span> <br/>
-           <ChangeStatus />
+           <ChangeStatus
+              status={item.status}
+              onChange={(newStatus) => updateStatus(index, newStatus)}
+            />
+
 
            </li>)}
           
       </ol>
-        
       </div>
 
     </div>
